2efe7486cbb2c14200555876de6976535f67b396
Prateekkumar <prateek.pritum@gmail.com>
2014-04-27 18:01:18 -0700

    adding system admin package

---
+++ src/SystemAdministrator/EmpSearchHold.java
@@ -0,0 +1,66 @@
+
+package SystemAdministrator;
+import javafx.beans.property.SimpleStringProperty;
+
+
+     	public class EmpSearchHold {
+
+		private SimpleStringProperty empid; 
+		private SimpleStringProperty password;
+		private SimpleStringProperty fname;
+		private SimpleStringProperty lname;
+		private SimpleStringProperty type;
+		
+		public EmpSearchHold()
+		{
+			this.empid = new SimpleStringProperty("");
+	    	this.password = new SimpleStringProperty("");
+	    	this.fname = new SimpleStringProperty("");
+	    	this.lname = new SimpleStringProperty("");
+	    	this.type =new SimpleStringProperty("");
+		}
+
+		public String getEmpid() {
+			return this.empid.get();
+		}
+
+		public void setEmpid(String empid) {
+			this.empid.set(empid);
+		}
+
+		public String getPassword() {
+			return this.password.get();
+		}
+
+		public void setPassword(String password) {
+			this.password.set(password);
+		}
+
+		public String getFname() {
+			return this.fname.get();
+		}
+
+		public void setFname(String fname) {
+			this.fname.set(fname);
+		}
+
+		public String getLname() {
+			return this.lname.get();
+		}
+
+		public void setLname(String lname) {
+			this.lname.set(lname);
+		}
+
+		public String getType() {
+			return this.type.get();
+		}
+
+		public void setType(String type) {
+			this.type.set(type);
+		}
+	
+}
+
+
+
